7f67017ce3c2fad7c28548d9e6e7508bf05901d7
deniz946 <deniz946@gmail.com>
2018-04-01 23:49:30 +0200

    Added address and coordinates to store creation form

--- models/Store.js
+++ models/Store.js
@@ -3,27 +3,47 @@ mongoose.Promise = global.Promise;
 const slug = require('slugs');
 
 const storeSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: 'Please enter a store name'
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name'
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true,
+  },
+  tags: [String],
+  created: {
+    type: Date,
+    default: new Date()
+  },
+  location: {
+    type: {
+      type: String,
+      default: 'Pointer'
     },
-    slug: String,
-    description: {
-        type: String,
-        trim: true,
-    },
-    tags: [String]
+    /** The array of coordinates stores in the first
+    index the longitude and in the second index the latitude **/
+    coordinates: [{
+      type: Number,
+      required: 'You must enter coordinates'
+    }],
+    address: {
+      type: String,
+      required: 'You must enter an address'
+    }
+  }
 })
 
 storeSchema.pre('save', function (next) {
-    // Just slugify if the name has changed
-    if (!this.isModified('name')) {
-        next()
-        return;
-    }
-    this.slug = slug(this.name);
-    next();
+  // Just slugify if the name has changed
+  if (!this.isModified('name')) {
+    next()
+    return;
+  }
+  this.slug = slug(this.name);
+  next();
 })
 
 module.exports = mongoose.model('Store', storeSchema);
