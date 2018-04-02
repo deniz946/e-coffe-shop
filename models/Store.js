a7c90192a79a7fa2802aac521999b548751afa59
deniz946 <deniz946@gmail.com>
2018-04-02 02:06:43 +0200

    Implemented a better way for generating slugs, avoiding duplicate slugs

--- models/Store.js
+++ models/Store.js
@@ -37,14 +37,20 @@ const storeSchema = new mongoose.Schema({
   }
 })
 
-storeSchema.pre('save', function (next) {
-  // Just slugify if the name has changed
+storeSchema.pre('save', async function(next) {
+  // Dont slugify if the name has not changed
   if (!this.isModified('name')) {
-    next()
+    next();
     return;
   }
   this.slug = slug(this.name);
+  // find other stores that have a slug of bulldog, bulldog-1, bulldog-2
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+  if(storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
   next();
-})
+});
 
 module.exports = mongoose.model('Store', storeSchema);
